969217c764b7333d6c5f6922a708249e8a6ef4f3
Alejandro Carrillo <carrilloalejandro0215@gmail.com>
2025-09-21 16:17:47 -0500

    Firestore is working storing data, and the date is being showed on the screen

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-analytics.js";
+import{
+    getFirestore,
+    collection,
+    addDoc,
+    onSnapshot
+} from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,31 +24,56 @@ measurementId: "G-GP321CS8PY"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 // const analytics = getAnalytics(app);
 
 
+
+
+
+
+
+
 //---------------------- M ------------------------------------
-
-
-
 let clienteID;
+
+setUpListenerSnapshot()
+clienteIdCheck()
 
 
 const botonSubmit = document.getElementById("botonSubmit")
 botonSubmit.addEventListener("click", sendMessage)
-clienteIdCheck()
-console.log(localStorage)
 
-function sendMessage(){
-    let messageSent = document.getElementById("messageInput")
-    console.log(messageSent.value)
-}
 
 function clienteIdCheck(){
     if(localStorage.getItem("clienteId") == null){
         clienteID = crypto.randomUUID()
         localStorage.setItem("clienteId", clienteID)
     }else{
-        console.log(localStorage)
+        clienteID = localStorage.getItem("clienteId");
     }
+}
+
+
+async function sendMessage(){
+    let messageSent = document.getElementById("messageInput")
+    console.log(messageSent.value)
+    let timenow = Date.now()
+    await addDoc(collection(db,"messages"),{
+        "text" : messageSent.value,
+        "userId" : String(clienteID),
+        "timestamp" : timenow
+    })
+}
+
+
+function setUpListenerSnapshot(){
+    onSnapshot(collection(db,"messages"), (snapshot) => {
+    snapshot.forEach(doc => {
+        let msg = document.createElement("p")
+        msg.textContent = `${doc.id} > ${doc.data().text}`
+        document.getElementById("chat").appendChild(msg)
+    });
+})
 }
